5b632ce173312668cd3ac52485223d20bd00e15c
Betabot18 <145249120+Betabot18@users.noreply.github.com>
2023-10-08 08:10:36 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.BOT_TOKEN = "6138612758:AAHkKB6qLIDbC0hUO6h9plaVhgef3Wzcta0" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "playmenu_boy" //your bot name
+global.OWNER_NAME = "ᴹᴿ᭄ Im its lena ×፝֟͜×" //your name
+global.OWNER_NUMBER = "6283823290735" //your telegram number
+global.OWNER = ["https://t.me/Myalena13", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
